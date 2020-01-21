eda91324d9d078b0e22deeb1bee191a652999e28
Vladimir Grichina <vgrichina@componentix.com>
2020-01-21 00:11:21 -0800

    Extract code evaluation into separate module

--- src/Block.svelte
+++ src/Block.svelte
@@ -29,15 +29,12 @@
 </style>
 
 <script context="module">
-    let evalContext = {
-        console: {}
-    };
 </script>
 
 <script>
-    import vm from 'vm';
     import { onMount } from 'svelte';
     import { notebook } from './stores';
+    import { evalCode } from './eval';
 
     import Result from './Result';
 
@@ -45,40 +42,6 @@
     export let index;
 
     let textarea;
-
-    function resultToJSON(result) {
-        console.log('resultToJSON', result);
-        // TODO: Different handlers for different JS objects
-        return {
-            type: 'Text',
-            text: `${result}`
-        }
-    }
-
-    function errorToJSON(error) {
-        return {
-            type: 'Error',
-            message: error.toString(),
-            stack: error.stack,
-            text: [`${error}`, error.stack].join('\n')
-        }
-    }
-
-    function evalCode(code) {
-        // TODO: Maintain global context
-        const logs = [];
-        // TODO: Proper console wrapper
-        evalContext.console.log = (...args) => {
-            logs.push(args.join(" "));
-            console.log(...args);
-        }
-        try {
-            const result = vm.runInNewContext(code, evalContext);
-            return { result: resultToJSON(result), logs };
-        } catch (e) {
-            return { result: errorToJSON(e), logs };
-        }
-    }
 
     function handleKeyPress(event) {
         console.log('key press', event);
@@ -104,6 +67,12 @@
         }
     }
 
+    function autoresizeTextarea() {
+        const HEIGHT_LIMIT = 200;
+        textarea.style.height = "";
+        textarea.style.height = Math.min(textarea.scrollHeight, HEIGHT_LIMIT) + "px";
+    }
+
     const updateResult = (index, { result, logs }) => ({ blocks, ...rest }) => ({
         ...rest,
         blocks: [...blocks.slice(0, index), { ...blocks[index], result, logs }, ...blocks.slice(index + 1)]
@@ -123,12 +92,13 @@
     })
 
     onMount(() => {
+        autoresizeTextarea();
         textarea.focus();
     });
 </script>
 
 <div class="code">
-    <textarea bind:value={block.code} on:keypress={handleKeyPress} on:keyup={handleKeyUp} bind:this={textarea} />
+    <textarea bind:value={block.code} on:keypress={handleKeyPress} on:keyup={handleKeyUp} on:input={autoresizeTextarea} bind:this={textarea} />
 </div>
 <div class="result">
     {#if block.logs}
---
+++ src/eval.js
@@ -0,0 +1,42 @@
+const vm = require('vm')
+
+let evalContext = {
+    console: {}
+};
+
+function resultToJSON(result) {
+    console.log('resultToJSON', result);
+    // TODO: Different handlers for different JS objects
+    return {
+        type: 'Text',
+        text: `${result}`
+    }
+}
+
+function errorToJSON(error) {
+    return {
+        type: 'Error',
+        message: error.toString(),
+        stack: error.stack,
+        text: [`${error}`, error.stack].join('\n')
+    }
+}
+
+function evalCode(code) {
+    // TODO: Maintain global context
+    const logs = [];
+    // TODO: Proper console wrapper
+    evalContext.console.log = (...args) => {
+        logs.push(args.join(" "));
+        console.log(...args);
+    }
+    try {
+        const result = vm.runInNewContext(code, evalContext);
+        return { result: resultToJSON(result), logs };
+    } catch (e) {
+        return { result: errorToJSON(e), logs };
+    }
+}
+
+
+module.exports = { evalCode }
